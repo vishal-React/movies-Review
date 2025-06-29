b45d3539a805dc130e29ab20239ea79c9ba41495
Vishal Gupta <vishalgupta28012004@gmail.com>
2025-06-29 19:04:05 +0530

    api baseurl change

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '1cc28d7cb8202fa7566afa90c4a8b9f4',
+    baseUrl: 'https://movies-backend-l13f.onrender.com/api/',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
